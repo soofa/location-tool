18ab3ef1fa4fb067cc9dab584d5213b89a7d5ac1
Stephen Karger <stephen.karger@gmail.com>
2017-05-21 21:46:02 -0400

    introduce bounding box entry template

--- location_tool/location_tool.py
+++ location_tool/location_tool.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from location_tool.database import init_db, db_session
+from location_tool import tasks
 
 app = Flask(__name__)
 app.config.from_object('config.default')
@@ -25,3 +26,7 @@ def initdb_command():
 @app.route('/')
 def location_tool():
     return render_template('HeatMap.html')
+
+@app.route('/bounding-box-entry')
+def bounding_box_entry():
+    return render_template('BoundingBox.html')
