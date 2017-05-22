2fc322818b594ffb4ad5593b47c3542ea7363aa3
Stephen Karger <stephen.karger@gmail.com>
2017-05-22 10:36:27 -0400

    add timestamp columns

--- location_tool/models.py
+++ location_tool/models.py
@@ -1,10 +1,20 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, DateTime, text
 from sqlalchemy.dialects import postgresql
+import datetime
 from location_tool.database import Base
 
 class BoundingBox(Base):
     __tablename__ = 'bounding_boxes'
     id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True),
+                        nullable=False,
+                        server_default=text("select current_timestamp at time zone 'UTC'")
+                        )
+    updated_at = Column(DateTime(timezone=True),
+                        nullable=False,
+                        server_default=text("select current_timestamp at time zone 'UTC'"),
+                        onupdate=text("select current_timestamp at time zone 'UTC'")
+                        )
     name = Column(Text, nullable=False)
     state = Column(Text, nullable=False)
     coordinates = Column(postgresql.JSON, nullable=False)
